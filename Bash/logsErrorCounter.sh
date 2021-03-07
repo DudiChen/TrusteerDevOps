ebee6b6bdb4cdc18ac249e25d25dcf15acc14f64
DudiChen <dudi.chen@gmail.com>
2021-03-07 09:11:25 +0200

    bash

--- Bash/logsErrorCounter.sh
+++ Bash/logsErrorCounter.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 LOGS_PATH=$1
+echo $1
 TEMP_FILE_PATH="/tmp/logFilesList"
 #LOGS_IN_PATH=$(ls -lhtr ${LOGS_PATH})
-ls -lhtr > ${TEMP_FILE_PATH}
+ls -lhtr $LOGS_PATH > ${TEMP_FILE_PATH}
