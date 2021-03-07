4723dd78f22524eab677a60612adabe535bd45ec
DudiChen <dudi.chen@gmail.com>
2021-03-07 15:45:44 +0200

    python q1 fix

--- Python/Q1/python_q1.py
+++ Python/Q1/python_q1.py
@@ -1,21 +1,31 @@
 import json
+import requests
 from flask import request, jsonify
 
 
 def inject_config_content():
-    config_file_location = "/data/python/config.json"
-    with open(config_file_location, "r+") as config_file:
-        config_data = json.load(config_file)
-        url = config_data["url"]
-        content_value = get_content_value_by_url(url)
-        config_data["content"] = content_value
-        json.dump(config_data, config_file)
+    # config_file_location = "/data/python/config.json"
+    # with open(config_file_location, "r+") as config_file:
+    #     config_data = json.load(config_file)
+    #     req_url = config_data["url"]
+    #     content_value = get_content_value_by_url(req_url)
+    #     config_data["content"] = content_value
+    #     json.dump(config_data, config_file)
+    country = "Israel"
+    server_url = "https://corona.lmao.ninja/v2/"
+    historical_request_suffix = "historical/"
+    req_url = server_url + historical_request_suffix + country
+    # country_data = {}
+
 
 
 def get_content_value_by_url(content_url):
-    # req_params = {}
+    # # req_params = {}
     content_length = 15
-    response = request.get(url=content_url)
+    # response = request.get(url=content_url)
+    req_params = {'lastdays': 30}
+    response = requests.get(url=content_url, params=req_params)
+    
     return response[:content_length]
 
 
