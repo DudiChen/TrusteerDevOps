a164ff0ed0f8d5fd18618f14d7870fabd7e669f8
DudiChen <dudi.chen@gmail.com>
2021-03-07 17:13:05 +0200

    ALL DONE

--- Bash/bash_q1.sh
+++ Bash/bash_q1.sh
@@ -2,13 +2,18 @@
 
 LOGS_PATH=$1
 TEMP_FILE_PATH="/tmp/logFilesList"
-# step1:
-ls -ld ${LOGS_PATH}/* | awk '{print $9}' | egrep '.*\.log$' > $TEMP_FILE_PATH
-# step2:
-cat $TEMP_FILE_PATH | xargs egrep -w -c '[_0-9]*ERROR[_0-9]*' | sed 's/:/ - /g'
-# NOTE: the ERROR regex is to avoid alphabetic words containing 'ERROR' unlike the work itself
 
-# rm -f $TEMP_FILE_PATH  # COMMENTED OUT IN CASE YOU"D LIKE TO EXAMINE THE TEMP FILE
+if [[ -d $LOGS_PATH ]]; then
+  # step1:
+  ls -ld ${LOGS_PATH}/* | awk '{print $9}' | egrep '.*\.log$' > $TEMP_FILE_PATH
+  # step2:
+  cat $TEMP_FILE_PATH | xargs egrep -w -c '[_0-9]*ERROR[_0-9]*' | sed 's/:/ - /g'
+  # NOTE: the ERROR regex is to avoid alphabetic words containing 'ERROR' unlike the work itself
+
+  # rm -f $TEMP_FILE_PATH  # COMMENTED OUT IN CASE YOU"D LIKE TO EXAMINE THE TEMP FILE
+else
+  echo "ERROR: Couldn't find the directory under the path giver: ${$LOGS_PATH}"
+fi
 
 # NOTE:
 # IF I WERE ASKED AS A ONE-LINER [not per assignment specification]:
