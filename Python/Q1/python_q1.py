f8c48d159290db7d22510b9157fa851edb98b7a6
DudiChen <dudi.chen@gmail.com>
2021-03-07 16:16:16 +0200

    python q1 fix

--- Python/Q1/python_q1.py
+++ Python/Q1/python_q1.py
@@ -7,13 +7,13 @@ def inject_config_content():
     config_file_location = "/data/python/config.json"
     with open(config_file_location, "r+") as config_file:
         config_data = json.load(config_file)
-        # req_url = config_data["url"]
-        # -------------------v
-        country = "Israel"
-        server_url = "https://corona.lmao.ninja/v2/"
-        historical_request_suffix = "historical/"
-        req_url = server_url + historical_request_suffix + country
-        # -------------------^
+        req_url = config_data["url"]
+        # # -------------------v
+        # country = "Israel"
+        # server_url = "https://corona.lmao.ninja/v2/"
+        # historical_request_suffix = "historical/"
+        # req_url = server_url + historical_request_suffix + country
+        # # -------------------^
         content_value = get_content_value_by_url(req_url)
         print("TEST: " + content_value)
         config_data["content"] = content_value
@@ -23,12 +23,12 @@ def inject_config_content():
 def get_content_value_by_url(content_url):
     # # req_params = {}
     content_length = 15
-    # response = request.get(url=content_url)
-    req_params = {'lastdays': 30}
-    response = requests.get(url=content_url, params=req_params)
+    response = request.get(url=content_url)
+    # req_params = {'lastdays': 30}
+    # response = requests.get(url=content_url, params=req_params)
     # TEST:
-    print("TYPE: " + str(type(response.text)))
-    print(str(response.text))
+    # print("TYPE: " + str(type(response.text)))
+    # print(str(response.text))
 
     return response.text[:content_length]
 
