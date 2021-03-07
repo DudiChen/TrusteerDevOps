f978b30aeec45d7e29e1facd519a14a63e249fba
DudiChen <dudi.chen@gmail.com>
2021-03-07 15:12:57 +0200

    BASH

--- Bash/logsErrorCounter.sh
+++ Bash/logsErrorCounter.sh
@@ -4,4 +4,10 @@ LOGS_PATH=$1
 echo $1
 TEMP_FILE_PATH="/tmp/logFilesList"
 #LOGS_IN_PATH=$(ls -lhtr ${LOGS_PATH})
-ls -l $LOGS_PATH | grep *.log | awk '{print $9}' > ${TEMP_FILE_PATH}
+ls -ld ${LOGS_PATH}/* | awk '{print $9}' | egrep '.*\.log$' > $TEMP_FILE_PATH
+cat $TEMP_FILE_PATH | xargs egrep -w -c '[_0-9]*ERROR[_0-9]*' | sed 's/:/ - /g'
+# rm -f $TEMP_FILE_PATH  # COMMENTED OUT IN CASE YOU"D LIKE TO EXAMINE THE TEMP FILE
+
+# NOTE:
+# IF I WERE ASKED AS A ONE-LINER [not per assignment specification]:
+# ls -ld ${LOGS_PATH}/* | awk '{print $9}' | egrep '.*\.log$' | xargs egrep -w -c '[_0-9]*ERROR[_0-9]*' | sed 's/:/ - /g'
